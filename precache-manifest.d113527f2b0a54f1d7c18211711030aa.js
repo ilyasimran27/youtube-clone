56f840e3acd2f9aa99a0479dd86a2ee7558ca7a5
Muhammad Imran <ilyasimran27@gmail.com>
2019-10-27 04:25:23 -0700

    Updates

---
+++ precache-manifest.d113527f2b0a54f1d7c18211711030aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec1f312b9988540b91bdebc40e4dab3",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "b92c25411261f8ed6b26",
+    "url": "/youtube-clone/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "043e217f70d7bd569f4b",
+    "url": "/youtube-clone/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "b92c25411261f8ed6b26",
+    "url": "/youtube-clone/static/js/2.7502c60f.chunk.js"
+  },
+  {
+    "revision": "043e217f70d7bd569f4b",
+    "url": "/youtube-clone/static/js/main.6757b17c.chunk.js"
+  },
+  {
+    "revision": "5485f6a73d9e30a1af1b",
+    "url": "/youtube-clone/static/js/runtime-main.8dfb5da2.js"
+  }
+]);
--- src/api/youtube.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const KEY = 'AIzaSyBV3Oyzn-E5GGhxkEY0L6trD8dXTo9quoQ';
-
-
-export default axios.create({
-    baseURL : 'https://www.googleapis.com/youtube/v3',
-    
-});
